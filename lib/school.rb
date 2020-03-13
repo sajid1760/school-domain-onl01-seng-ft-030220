c5624ae84d520f47e38fcd2a92e592aa68fb619c
Sajid Anjum <sajid1760@gmail.com>
2020-03-13 23:31:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,16 +2,13 @@ class School
   
   ROSTER = {}
   
-  a = 1
-  12.times do
-    ROSTER[a] = []
-  end
   
   def initialize(name)
     @name = name
   end
   
   def add_student(stdname, grade)
+    ROSTER[grade] = []
     ROSTER[grade] << stdname
   end
   
