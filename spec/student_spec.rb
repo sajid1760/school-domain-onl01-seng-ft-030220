9993d19edad72acd3686262335a63d0c25d4d704
Sajid Anjum <sajid1760@gmail.com>
2020-03-13 23:57:11 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -36,10 +36,10 @@ describe 'School' do
 
   describe '#grade' do
     it 'is able to retreive students from a grade' do
-      @school.add_student("Homer Simpson", 9)
-      @school.add_student("Avi Flombaum", 10)
-      @school.add_student("Jeff Baird", 10)
-      @school.add_student("Blake Johnson", 7)
+     # @school.add_student("Homer Simpson", 9)
+     # @school.add_student("Avi Flombaum", 10)
+     # @school.add_student("Jeff Baird", 10)
+     # @school.add_student("Blake Johnson", 7)
 
       expect(@school.grade(10)).to eq(["AC Slater","Jeff Baird","Blake Johnson", "Avi Flombaum"])
     end
