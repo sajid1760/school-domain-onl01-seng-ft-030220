2f980f7dc1f5129a3bb35ca6be6b2a8da9297977
Sajid Anjum <sajid1760@gmail.com>
2020-03-13 23:20:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,19 @@ class School
   end
   
   def add_student(stdname, grade)
+    if @@roster[grade] = nil then @@roster[grade] = [] end
     @@roster[grade] << stdname
   end
   
+  def roster
+    roster
+  end
+  
+  def grade(grade)
+    @@roster[grade]
+  end
+  
+  def sort 
+    
+    
   
