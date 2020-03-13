35b0bba8262d60b62bd6c27ed365535dbaa3cfd8
Sajid Anjum <sajid1760@gmail.com>
2020-03-13 23:16:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  
+  @@roster = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_student(stdname, grade)
+    @@roster[grade] << stdname
+  end
+  
+  
