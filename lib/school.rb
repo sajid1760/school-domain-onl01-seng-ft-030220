ce6aa1205c941bb1357faa9574f6f411d03e9ed1
Sajid Anjum <sajid1760@gmail.com>
2020-03-14 00:02:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -35,11 +35,11 @@ class School
   
 end
 
-school = School.new("Bayside High School")
-school.add_student("Zach Morris", 9)
-school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-school.roster
-school.grade(9)
-school.sort 
+#school = School.new("Bayside High School")
+#3school.add_student("Zach Morris", 9)
+#school.add_student("AC Slater", 9)
+#school.add_student("Kelly Kapowski", 10)
+#school.add_student("Screech", 11)
+#school.roster
+#school.grade(9)
+#school.sort 
